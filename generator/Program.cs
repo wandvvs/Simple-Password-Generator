559a9a3938b06edd7f1d866378a051bed321338e
wand <wand@DESKTOP-4FS8GTI>
2023-01-17 17:30:32 +0300

    Добавьте файлы проекта.

---
+++ generator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+Console.WriteLine("Вас приветствует генератор паролей!\n");
+
+Console.Write("Введите длину пароля: ");
+int length = int.Parse(Console.ReadLine());
+
+const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; 
+StringBuilder password = new StringBuilder();
+Random rnd = new Random();
+
+for (int i = 0; i < length; i++)
+{
+    password.Append(valid[rnd.Next(valid.Length)]);
+}
+password.ToString();
+Console.WriteLine(password);
